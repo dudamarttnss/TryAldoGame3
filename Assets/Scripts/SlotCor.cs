42bc36ef63a8432e8347cf04e2b332ca86cd2eb3
Samuel-Lopes-projetos <samuellopesdossantosneto@gmail.com>
2026-05-23 23:26:02 -0300

    atulização so script para acender

--- Assets/Scripts/SlotCor.cs
+++ Assets/Scripts/SlotCor.cs
@@ -8,6 +8,9 @@ public class SlotCor : MonoBehaviour
     [Header("Objeto Atual")]
     public CorArrastavel imagemAtual;
 
+    [Header("Indicador")]
+    public SpriteRenderer indicador;
+
     public bool VerificarCor()
     {
         if (imagemAtual == null)
@@ -16,6 +19,16 @@ public class SlotCor : MonoBehaviour
         bool correto = imagemAtual.cor == cor;
 
         Debug.Log("Cor correta? " + correto);
+
+        // Se estiver correto fica amarelo
+        if (correto)
+        {
+            indicador.color = Color.yellow;
+        }
+        else
+        {
+            indicador.color = Color.white;
+        }
 
         return correto;
     }
